c0c14ff17f6fe330a253ad3d78d96ee14b8478c8
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-12-18 11:39:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        br.close();
+        int res = (int)A.charAt(0);
+        System.out.println(res);
+    }   
+}
