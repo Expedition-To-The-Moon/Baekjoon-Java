09527f014dddc79389d1c6c144727eb107699142
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2024-01-03 11:04:55 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86224 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int cnt = 0;
+		int num = 665;
+
+		while (cnt != N) {
+			num++;
+
+			if (String.valueOf(num).contains("666")) {
+				cnt++;
+			}
+		}
+		System.out.println(num);
+    }
+}
