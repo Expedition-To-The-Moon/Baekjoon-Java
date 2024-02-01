792a32cde6dd6020cf19d9aa60307794463ce2d5
ExpeditionMoon <128493900+ExpeditionMoon@users.noreply.github.com>
2024-02-01 14:59:45 +0900

    [Silver III] Title: 통계학, Time: 584 ms, Memory: 49524 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+        avg();
+        median();
+        mode();
+        scope();
+    }
+
+    static void avg() {
+        double sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        System.out.println((int) Math.round(sum / nums.length));
+    }
+
+    static void median() {
+        Arrays.sort(nums);
+        int idx = nums.length / 2;
+        System.out.println(nums[idx]);
+    }
+
+    static void mode() {
+        int[] modeNum = new int[80001];
+        for (int i : nums) {
+            modeNum[i + 4000]++;
+        }
+        int max = Arrays.stream(modeNum).max().getAsInt();
+
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < modeNum.length; i++) {
+            if (modeNum[i] == max) {
+                num.add(i - 4000);
+            }
+        }
+        if (num.size() == 1) {
+            System.out.println(num.get(0));
+        } else {
+            System.out.println(num.get(1));
+        }
+    }
+
+    static void scope() {
+        Arrays.sort(nums);
+        if (nums.length == 1) {
+            System.out.println(0);
+        } else {
+            int min = nums[0];
+            int max = nums[nums.length - 1];
+            if (min <= 0 && max >= 0) {
+                System.out.println(Math.abs(min) + Math.abs(max));
+            } else {
+                System.out.println(max - min);
+            }
+        }
+    }
+}
