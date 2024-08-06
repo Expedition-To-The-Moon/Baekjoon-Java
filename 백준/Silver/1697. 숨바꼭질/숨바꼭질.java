bd5b186766577062a92358d67de17f18a52e0f3d
ExpeditionMoon <128493900+ExpeditionMoon@users.noreply.github.com>
2024-08-06 17:31:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        if (n == k) {
+            System.out.println(0);
+            return;
+        }
+
+        boolean[] visited = new boolean[100001];
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(n);
+        visited[n] = true;
+
+        int cnt = 0;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                int pos = que.poll();
+
+                if (pos == k) {
+                    System.out.println(cnt);
+                    return;
+                }
+                if (pos - 1 >= 0 && !visited[pos - 1]) {
+                    que.offer(pos - 1);
+                    visited[pos - 1] = true;
+                }
+                if (pos + 1 <= 100000 && !visited[pos + 1]) {
+                    que.offer(pos + 1);
+                    visited[pos + 1] = true;
+                }
+                if (pos * 2 <= 100000 && !visited[pos * 2]) {
+                    que.offer(pos * 2);
+                    visited[pos * 2] = true;
+                }
+            }
+            cnt++;
+        }
+    }
+}
