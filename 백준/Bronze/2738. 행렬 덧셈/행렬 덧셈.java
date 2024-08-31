7b7ed243092d241d8fbb8dd238ef56bdd824a407
ExpeditionMoon <128493900+ExpeditionMoon@users.noreply.github.com>
2024-08-31 12:27:58 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 148 ms, Memory: 16832 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] nArr = new int[n][m];
+        int[][] mArr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                nArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                mArr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int sum = nArr[i][j] + mArr[i][j];
+                sb.append(sum).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
